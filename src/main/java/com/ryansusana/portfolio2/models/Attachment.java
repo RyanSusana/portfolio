f63a836823bf618b81d1e54f23c207b617b356eb
Ryan Susana <ryansusana@live.com>
2020-06-07 15:39:47 +0200

    Added filereference to attachment

--- src/main/java/com/ryansusana/portfolio2/models/Attachment.java
+++ src/main/java/com/ryansusana/portfolio2/models/Attachment.java
@@ -1,6 +1,7 @@
 package com.ryansusana.portfolio2.models;
 
 import com.elepy.annotations.Featured;
+import com.elepy.annotations.FileReference;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +10,7 @@ import lombok.Setter;
 public class Attachment {
     @Featured
     private String name;
+
+    @FileReference
     private String file;
 } 
