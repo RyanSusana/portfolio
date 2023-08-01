38ec41ff20ab24a973a4ccd4c29395596c034a43
Ryan Susana <ryansusana97@gmail.com>
2023-08-02 00:52:26 +0200

    Switched to yaml file

--- src/main/java/com/ryansusana/portfolio2/Main.java
+++ src/main/java/com/ryansusana/portfolio2/Main.java
@@ -20,23 +20,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         final Configuration mongoConfig;
-
-        var autoConfiguredOtel = AutoConfiguredOpenTelemetrySdk.initialize();
-        var otel = autoConfiguredOtel.getOpenTelemetrySdk();
+//
+//        var autoConfiguredOtel = AutoConfiguredOpenTelemetrySdk.initialize();
+//        var otel = autoConfiguredOtel.getOpenTelemetrySdk();
 
 
         if ("TEST".equals(System.getenv("environment"))) {
             mongoConfig = MongoConfiguration.inMemory();
-            Tracer tracer =
-                    otel.getTracer("instrumentation-library-name", "1.0.0");
-            var childSpan = tracer.spanBuilder("child")
-//                    .setParent(Context.current().with(parentSpan))
-                    .startSpan();
-            try {
-                // do stuff
-            } finally {
-                childSpan.end();
-            }
+//            Tracer tracer =
+//                    otel.getTracer("instrumentation-library-name", "1.0.0");
+//            var childSpan = tracer.spanBuilder("child")
+////                    .setParent(Context.current().with(parentSpan))
+//                    .startSpan();
+//            try {
+//                // do stuff
+//            } finally {
+//                childSpan.end();
+//            }
         } else {
             final var rawUri = System.getenv("DATABASE_CONNECTION_STRING");
             MongoClientURI uri = new MongoClientURI(rawUri);
